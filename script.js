8a979c892633d15054c867d351bee59f5c54c9dd
Luke Saccento <lukesaccento@icloud.com>
2023-04-27 18:15:30 -0700

    basically finished the framework styling

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("navbar");
-  if (x.className === "navbar") {
-    x.className += " responsive";
-  } else {
-    x.className = "navbar";
-  }
-} 
+window.onload = getServerStatus();
+function getServerStatus() {
+  console.log("getting status");
+  $.getJSON('https://api.mcsrvstat.us/2/mc.lukesmp.net', function(data) {
+    if (data.motd.clean[0].includes("Offline")) {
+      // document.getElementById("server-status").innerHTML = "Offline";
+    }
+    //if server is online
+    if (data.online == true) {
+      //if server is online and has players
+      if (data.players.online < 1) {
+        //display server status
+        
+      }
+    }
+    if (data.players.max > 0) {
+      document.getElementById("server-status").innerHTML += data.players.online + "/" + data.players.max;
+    }
+  });
+}
